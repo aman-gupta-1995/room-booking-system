4f922fd8d55fcfe1d66ced3764775607d712f99f
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-03-29 13:58:28 +0530

    Update evdtls.php

--- Calendar/evdtls.php
+++ Calendar/evdtls.php
@@ -11,7 +11,7 @@
  
 <!DOCTYPE html> 
     <html> 
-    <head>
+    <head> 
         <title>Template</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
