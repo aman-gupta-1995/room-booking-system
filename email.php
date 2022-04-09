5274bf0a117b75ecb4effcf83d7e176ec461dbfd
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-04-09 08:18:23 +0530

    Update email.php

--- email.php
+++ email.php
@@ -5,7 +5,7 @@ $subject = "email try";
 $message = "heyyyyy";
 $header = "From: someone@mail.com";
 
-if ( mail($to, $subject, $message, $header) ) {
+if ( mail($to, $subject, $message, $header) ) { 
 	echo "email has been sent"; 
 } else
 echo "not sent";
