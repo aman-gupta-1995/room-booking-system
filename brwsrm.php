966d532d5b2eb4b65c24a17a7a794ed07cad6c8b
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-07-18 00:14:30 +0530

    Update brwsrm.php

--- brwsrm.php
+++ brwsrm.php
@@ -1,6 +1,6 @@
 <?php
 	require_once 'cnntdb.php'; 
-   
+    
 	connect();         
      
 	$sql = "SELECT Room.Rname, Rtype, Rcapacity, Rdetails, Rprice, fdate, tdate FROM Room, BkingDetail WHERE Room.Rname = BkingDetail.Rname;";
